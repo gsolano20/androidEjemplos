0d3822a1a4ba2a7994626a1bb4e97e07cff57867
Gustavo solano <gsolano20@gmail.com>
2020-01-31 14:30:15 -0600

    navigation bar example

---
+++ NavigationDrawer/NavigationDrawer/app/src/main/java/com/journaldev/navigationdrawer/ConnectFragment.java
@@ -0,0 +1,26 @@
+package com.journaldev.navigationdrawer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * Created by anupamchugh on 10/12/15.
+ */
+public class ConnectFragment extends Fragment {
+
+    public ConnectFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_connect, container, false);
+
+        return rootView;
+    }
+
+}
---
+++ NavigationDrawer/NavigationDrawer/app/src/main/java/com/journaldev/navigationdrawer/FixturesFragment.java
@@ -0,0 +1,26 @@
+package com.journaldev.navigationdrawer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * Created by anupamchugh on 10/12/15.
+ */
+public class FixturesFragment extends Fragment {
+
+    public FixturesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_fixtures, container, false);
+
+        return rootView;
+    }
+
+}
